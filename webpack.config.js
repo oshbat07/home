e56262d8a7aa06cea4dd735880fe710360e96c0c
Oshinb15 <batraoshin15061995@gmail.com>
2023-07-25 18:25:01 +0530

    publicpath changed

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "http://localhost:3000/",
+    publicPath:
+      argv.mode === "development"
+        ? "http://localhost:3000/"
+        : "https://home-zzrq-oshinbatra07-gmailcom.vercel.app/",
   },
 
   resolve: {
