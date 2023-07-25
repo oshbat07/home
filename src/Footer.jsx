08abde9fa28d500a2299ff29668cc8d63930d06a
Oshinb15 <batraoshin15061995@gmail.com>
2023-07-25 18:49:55 +0530

    header-footer updated

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="p-5 bg-blue-500 text-white -text-3xl font-bold">
-      Micro Frontend Footer
+      MFE FOOTER
     </div>
   );
 };
